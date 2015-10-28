3affe69bc19b96b510f29591e3d50a6c43cd1e0e
Charlie Ledogar <cledogar@twistbioscience.com>
2015-10-27 23:18:25 -0700

    Add more columns to Plate Details report.

--- migrations/versions/558be9081c47_gene_assembly_sample_view.py
+++ migrations/versions/558be9081c47_gene_assembly_sample_view.py
@@ -34,6 +34,25 @@ def upgrade():
         -- SAMP.fwd_primer_ps_id as sample_fwd_primer_ps_id,
         -- SAMP.rev_primer_ps_id as sample_rev_primer_ps_id,
         -- GA.parent_sample_id as ga_parent_sample_id,
+        COR.order_id as cor_order_id,
+        COR.order_date as cor_order_date,
+        COR.customer_id as cor_customer_id,
+        CID.institution_id as cid_institution_id,
+        COI.order_item_id as coi_order_item_id,
+        COI.line_item_number as coi_line_item_number,
+        COI.order_configuration_id as coi_order_configuration_id,
+        COI.received_datetime as coi_received_datetime,
+        COI.due_datetime as coi_due_datetime,
+        COI.priority as coi_priority,
+        COI.order_item_status_id as coi_order_item_status_id,
+        COI.order_item_type_id as coi_order_item_type_id,
+        COI.order_item_delivery_format_id as coi_order_item_delivery_format_id,
+        COI.customer_sequence_num as coi_customer_sequence_num,
+        COI.customer_line_item_id as coi_customer_line_item_id,
+        COI.customer_line_item_description as coi_customer_line_item_description,
+        COI.description as coi_description,
+        COI.notes as coi_notes,
+        -- COI.status_notes as coi_status_notes,
         GA.sagi_id as ga_sagi_id,
         SAGI.sag_id as sagi_sag_id,
         SAGI.date_created as sagi_date_created,
@@ -44,12 +63,21 @@ def upgrade():
         SAGG.fivep_as_dir as sagg_fivep_as_dir,
         SAGG.threep_as_id as sagg_threep_as_id,
         SAGG.threep_as_dir as sagg_threep_as_dir,
-        GS.sequence_id as gs_sequence_id
+        GS.sequence_id as gs_sequence_id,
+        SEQ.seq as gs_seq,
+        SEQ.name as gs_name,
+        SEQ.description as gs_description
         from gene_assembly_sample GA
         inner join sample SAMP on SAMP.sample_id = GA.sample_id
         inner join sag_instance SAGI on SAGI.sagi_id = GA.sagi_id
         inner join sag_node_gene SAGG on SAGG.sag_id = SAGI.sag_id
         inner join gene_sequence GS on GS.sequence_id = SAGG.sequence_id
+        inner join sequence SEQ on SEQ.sequence_id = GS.sequence_id
+        inner join synthesis_cluster_sample_join SCSJ on SCSJ.sample_id = GA.sample_id
+        inner join synthesis_cluster CLST on CLST.cluster_id = SCSJ.cluster_id -- and CLST.sagi_id = SAGI.sagi_id
+        left outer join order_item COI on COI.order_item_id = CLST.order_item_id
+        left outer join "order" COR on COR.order_configuration_id = COI.order_configuration_id
+        left outer join customer CID on CID.customer_id = COR.customer_id
         left outer join operator OP on OP.operator_id = SAMP.operator_id
         where SAMP.type_id='gene_assembly';
         """
