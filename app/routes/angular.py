2e0bfc85271086288f75f350f17827ebf84230ee
Charlie Ledogar <cledogar@twistbioscience.com>
2015-10-29 17:29:02 -0700

    Add cloning_process_id to plate sample details.

--- app/routes/angular.py
+++ app/routes/angular.py
@@ -558,6 +558,7 @@ def plate_details(sample_plate_barcode, format, basicDataOnly=False):
         'coi_customer_line_item_description',
         'coi_description',
         'coi_notes',
+        'clo_cloning_process_id',
         'ga_sagi_id',
         'sagi_sag_id',
         'sagi_date_created',
