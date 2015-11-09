850761a9e5502d9ca094f5bd4844892806330b21
Andrew Stewart <astewart@twistbioscience.com>
2015-11-09 13:14:11 -0800

    Review for #MES-405
    Passes test upgrade on shared dev

--- migrations/versions/2451e6566f47_create_sample_status_table.py
+++ migrations/versions/2451e6566f47_create_sample_status_table.py
@@ -17,20 +17,20 @@ from sqlalchemy.dialects import postgresql
 def upgrade():
 
     op.create_table(
+        'sample_status_code',
+        sa.Column('status_type', sa.String(40), nullable=False),
+        sa.Column('description', sa.String(512), nullable=True),
+        sa.Column('date_added', sa.TIMESTAMP, nullable=False),
+        sa.PrimaryKeyConstraint('status_type')
+    )
+
+    op.create_table(
         'sample_status',
         sa.Column('sample_id', sa.String(40), sa.ForeignKey('sample.sample_id'), nullable=False),
         sa.Column('status_type', sa.String(40), sa.ForeignKey('sample_status_code.status_type'), nullable=False),
         sa.Column('status_date', sa.TIMESTAMP, nullable=False),
         sa.Column('operator_id', sa.String(100), nullable=False),
         sa.PrimaryKeyConstraint('sample_id')
-    )
-
-    op.create_table(
-        'sample_status_code',
-        sa.Column('status_type', sa.String(40), nullable=False),
-        sa.Column('description', sa.String(512), nullable=True),
-        sa.Column('date_added', sa.TIMESTAMP, nullable=False),
-        sa.PrimaryKeyConstraint('status_type')
     )
 
 def downgrade():
