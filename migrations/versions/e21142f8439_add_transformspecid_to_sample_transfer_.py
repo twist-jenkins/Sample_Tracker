d857e5ab4d98647b73da4c0322824fcd8fb26b2e
Charlie Ledogar <cledogar@twistbioscience.com>
2015-11-11 15:57:26 -0800

    Placeholder.

---
+++ migrations/versions/e21142f8439_add_transformspecid_to_sample_transfer_.py
@@ -0,0 +1,22 @@
+"""add transformSpecId to sample_transfer table
+
+Revision ID: e21142f8439
+Revises: 3cad6c561c85
+Create Date: 2015-11-11 10:25:32.817750
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e21142f8439'
+down_revision = '3cad6c561c85'
+
+from alembic import op
+import sqlalchemy as db
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
