786b1df428592afc957a497145ffc78e374182a3
Charlie Ledogar <cledogar@twistbioscience.com>
2015-12-11 16:46:53 -0800

    Add analytic query as a view.

--- migrations/versions/198099a0ee4e_add_preceding_transfer_type_id_column.py
+++ migrations/versions/198099a0ee4e_add_preceding_transfer_type_id_column.py
@@ -16,17 +16,46 @@ import sqlalchemy as db
 
 def upgrade():
     op.add_column('sample_transfer_type',
-                  db.Column('preceding_transfer_type_id',
+                  db.Column('prior_transfer_type_id',
                             db.Integer(),
                             db.ForeignKey('sample_transfer_type.id')))
 
     op.execute("update sample_transfer_type"
-               " set preceding_transfer_type_id = id + 1"
-               " where id + 1 in (select id from sample_transfer_type)")
+               " set prior_transfer_type_id = id - 1"
+               " where id - 1 in (select id from sample_transfer_type)")
+
+    prior_sql = """
+      create view sampletrack.plate_prior_step_view as
+      select * from (
+          Select distinct
+              sp.type_id,
+              sp.external_barcode,
+              sp.status,
+              det.sample_transfer_id,
+              det.destination_sample_plate_id,
+              st.sample_transfer_type_id,
+              st.date_transfer,
+              stt.name,
+              stt.sample_transfer_template_id,
+              stt.prior_transfer_type_id,
+              Row_Number() Over ( Partition By det.destination_sample_plate_id
+                                  Order By date_transfer ) As rnk
+          From  sample_transfer_detail det
+          join sample_transfer st on st.id = det.sample_transfer_id
+          join sample_transfer_type stt on stt.id = st.sample_transfer_type_id
+          join sample_plate sp on sp.sample_plate_id = det.destination_sample_plate_id
+      ) as subq
+      where rnk = 1; -- most recent step
+    """
+    op.execute(prior_sql)
 
 
 def downgrade():
+    op.execute("drop view if exists plate_prior_step_view")
+
     op.execute("alter table sample_transfer_type"
                " drop column if exists "
-               " preceding_transfer_type_id")
+               " prior_transfer_type_id")
+
+
 
