d030ed921c6f719f4c59ab9419c3f06b54d0b525
Charlie Ledogar <cledogar@twistbioscience.com>
2015-12-15 14:56:04 -0800

    Migration for add_hamilton_transform_steps

---
+++ migrations/versions/29c899083825_add_hamilton_transform_steps.py
@@ -0,0 +1,22 @@
+"""add_hamilton_transform_steps
+
+Revision ID: 29c899083825
+Revises: 3111d557c83a
+Create Date: 2015-12-15 14:55:15.359919
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '29c899083825'
+down_revision = '3111d557c83a'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
