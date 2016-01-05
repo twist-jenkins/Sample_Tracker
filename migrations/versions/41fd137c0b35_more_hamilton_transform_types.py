c1c74c8abdcca3df9effe08c5ee4ab0f8fcb6bb1
Charlie Ledogar <cledogar@twistbioscience.com>
2016-01-05 14:37:24 -0800

    Whitespace fixes.

--- migrations/versions/41fd137c0b35_more_hamilton_transform_types.py
+++ migrations/versions/41fd137c0b35_more_hamilton_transform_types.py
@@ -15,25 +15,25 @@ import sqlalchemy as sa
 
 
 def upgradeTransferTemplates():
-    update_sql = ("update sample_transfer_template "
-                  "set name = :name, "
-                  "is_one_to_one_transfer = :ioto, "
-                  "source_plate_well_count = :spwc, "
-                  "destination_plate_well_count = :dpwc "
-                  "where id = :id")
+    update_sql = ("update sample_transfer_template"
+                  " set name = :name,"
+                  " is_one_to_one_transfer = :ioto,"
+                  " source_plate_well_count = :spwc,"
+                  " destination_plate_well_count = :dpwc"
+                  " where id = :id")
 
-    insert_sql = ("insert into sample_transfer_template  "
-                  "(id, name, is_one_to_one_transfer, source_plate_well_count, "
-                  " destination_plate_well_count) "
-                  "select :id, :name, :ioto, :spwc, :dpwc "
-                  "where not exists "
-                  "(select * from sample_transfer_template where id = :id)")
+    insert_sql = ("insert into sample_transfer_template"
+                  " (id, name, is_one_to_one_transfer, source_plate_well_count,"
+                  " destination_plate_well_count)"
+                  " select :id, :name, :ioto, :spwc, :dpwc"
+                  " where not exists"
+                  " (select * from sample_transfer_template where id = :id)")
 
     sql_param_names = ("id", "name", "ioto", "spwc", "dpwc")
 
     desired_values = [
-        [28, "Hitpicking for Shipping in Plates", "F", None, None]
-        ,[33, "Hitpicking for Shipping in Tubes", "F", None, None]
+        [28, "Hitpicking for Shipping in Plates", "F", None, None],
+        [33, "Hitpicking for Shipping in Tubes", "F", None, None]
     ]
 
     for row_values in desired_values:
@@ -43,28 +43,28 @@ def upgradeTransferTemplates():
         # for insert and update should allow this without causing failures.
         op.execute(sa.text(update_sql).bindparams(**args))
         op.execute(sa.text(insert_sql).bindparams(**args))
+
 
 def upgradeTransferTypes():
-    update_sql = ("update sample_transfer_type "
-              "set name = :name, "
-              "sample_transfer_template_id = :stti, "
-              "source_plate_count = :spc, "
-              "destination_plate_count = :dpc, "
-              "prior_transfer_type_id = :ptti"
-              "where id = :id")
+    update_sql = ("update sample_transfer_type"
+                  " set name = :name,"
+                  " sample_transfer_template_id = :stti,"
+                  " source_plate_count = :spc,"
+                  " destination_plate_count = :dpc"
+                  " where id = :id")
 
-    insert_sql = ("insert into sample_transfer_type "
-                  "(id, name, sample_transfer_template_id, "
-                  " source_plate_count, destination_plate_count, prior_transfer_type_id) "
-                  "select :id, :name, :stti, :spc, :dpc, :ptti "
-                  "where not exists "
-                  "(select * from sample_transfer_type where id = :id)")
+    insert_sql = ("insert into sample_transfer_type"
+                  " (id, name, sample_transfer_template_id,"
+                  " source_plate_count, destination_plate_count)"
+                  " select :id, :name, :stti, :spc, :dpc"
+                  " where not exists"
+                  " (select * from sample_transfer_type where id = :id)")
 
-    sql_param_names = ("id", "name", "stti", "spc", "dpc", "ptti")
+    sql_param_names = ("id", "name", "stti", "spc", "dpc")
 
     desired_values = [
-        [48, "Hitpicking for Shipping in Plates", 28, 1, 1, 47]
-        ,[51, "Hitpicking for Shipping in Tubes", 33, 0, 0, 47]
+        [48, "Hitpicking for Shipping in Plates", 28, 1, 1, 47],
+        [51, "Hitpicking for Shipping in Tubes", 33, 0, 0, 47]
     ]
 
     for row_values in desired_values:
@@ -74,11 +74,12 @@ def upgradeTransferTypes():
         # for insert and update should allow this without causing failures.
         op.execute(sa.text(update_sql).bindparams(**args))
         op.execute(sa.text(insert_sql).bindparams(**args))
-        
+
+
 def upgrade():
 
-    upgradeTransferTemplates();
-    upgradeTransferTypes();
+    upgradeTransferTemplates()
+    upgradeTransferTypes()
 
 
 def downgrade():
