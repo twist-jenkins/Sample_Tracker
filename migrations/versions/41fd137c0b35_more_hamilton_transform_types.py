9f9c8e0e2c2a7ddf37d22652e3469e16a17171b3
Charlie Ledogar <cledogar@twistbioscience.com>
2016-01-05 13:54:55 -0800

    Migration for More Hamilton transform types.

---
+++ migrations/versions/41fd137c0b35_more_hamilton_transform_types.py
@@ -0,0 +1,22 @@
+"""More Hamilton transform types.
+
+Revision ID: 41fd137c0b35
+Revises: 29c899083825
+Create Date: 2016-01-05 13:54:12.114773
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '41fd137c0b35'
+down_revision = '29c899083825'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
