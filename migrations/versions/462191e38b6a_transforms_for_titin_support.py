2b13bb27f4d04b368874b8db827900533dae41e0
Charlie Ledogar <cledogar@twistbioscience.com>
2016-01-11 14:32:58 -0800

    Titin support within Sample Tracker.

---
+++ migrations/versions/462191e38b6a_transforms_for_titin_support.py
@@ -0,0 +1,22 @@
+"""Transforms for Titin support.
+
+Revision ID: 462191e38b6a
+Revises: 430e2caa4545
+Create Date: 2016-01-11 14:28:38.583640
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '462191e38b6a'
+down_revision = '430e2caa4545'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
