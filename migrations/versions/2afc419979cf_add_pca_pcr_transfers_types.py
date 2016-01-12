103b87de548bc04bdd82eef1300d0952d06a563d
Charlie Ledogar <cledogar@twistbioscience.com>
2016-01-12 15:21:31 -0800

    Two new migrations for Titin.

---
+++ migrations/versions/2afc419979cf_add_pca_pcr_transfers_types.py
@@ -0,0 +1,22 @@
+"""add pca pcr transfers types
+
+Revision ID: 2afc419979cf
+Revises: 462191e38b6a
+Create Date: 2016-01-12 15:20:07.554481
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2afc419979cf'
+down_revision = '462191e38b6a'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
