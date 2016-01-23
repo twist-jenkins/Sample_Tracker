283a98855e09bf781fddb484edb1cad53128983f
James Diggans <jdiggans@twistbioscience.com>
2016-01-22 17:12:50 -0800

    Cleaning up with Charlie

--- manage.py
+++ manage.py
@@ -13,8 +13,8 @@ from flask.ext.script import Manager, Shell, Server
 
 from flask.ext.migrate import Migrate, MigrateCommand
 
-from scripts import (CreateAllTables, LoadLookupTables, DropAllTables,
-    DeleteSampleTransfer, ImportSampleTransferTemplate)
+# from scripts import (CreateAllTables, LoadLookupTables, DropAllTables,
+#     DeleteSampleTransfer, ImportSampleTransferTemplate)
 
 from app import app, db
 
@@ -29,21 +29,21 @@ manager.add_command('db', MigrateCommand)
 manager.add_command("runserver", Server())
 manager.add_command("shell", Shell())
 
-manager.add_command('createalltables', CreateAllTables())
-manager.add_command('loadlookuptables', LoadLookupTables())
-manager.add_command('dropalltables', DropAllTables())
+# manager.add_command('createalltables', CreateAllTables())
+# manager.add_command('loadlookuptables', LoadLookupTables())
+# manager.add_command('dropalltables', DropAllTables())
 
 #
 # Example: python manage.py deletesampletransfer -id 3
 #
-manager.add_command('deletesampletransfer', DeleteSampleTransfer())
+# manager.add_command('deletesampletransfer', DeleteSampleTransfer())
 
 #
 # Example: python manage.py importtemplate -f TemplateExample.xlsx -n "the template name"
 #
 # python manage.py importtemplate -f Template384to48.csv -n "384 to 48"
 #
-manager.add_command('importtemplate', ImportSampleTransferTemplate())
+# manager.add_command('importtemplate', ImportSampleTransferTemplate())
 
 
 @manager.command
