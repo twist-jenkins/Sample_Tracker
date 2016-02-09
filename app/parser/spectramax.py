27b7bb26bfc4cad7a87bd78c9444a1b4d84fc47b
James Diggans <jdiggans@twistbioscience.com>
2016-02-09 00:37:51 -0800

    File is UTF-8 from the UI but UTF-16LE on disk so handle both

--- app/parser/spectramax.py
+++ app/parser/spectramax.py
@@ -7,17 +7,25 @@ quantitation (in ng/ul) for the samples in the measured 384-well plate.
 import re
 
 AWKWARD_ENCODING = 'utf-16-le'
-BLOCK_DIVIDER = "~End".encode(AWKWARD_ENCODING)
+BLOCK_DIVIDER = "~End"
+BLOCK_DIVIDER_UTF16 = BLOCK_DIVIDER.encode(AWKWARD_ENCODING)
 GROUP_ID = "Group: Unknowns".encode(AWKWARD_ENCODING)
 BLOCK_END = "Group Summaries".encode(AWKWARD_ENCODING)
 OUTSIDE_LADDER = "Range?".encode(AWKWARD_ENCODING)
 
 
-def parse(fcontents):
-    """Parse the file (as a string in fcontents)."""
-    # Handle a bunch of Unicode nonsense; files appear to be in UTF-16LE
-    quant_results = fcontents.split(BLOCK_DIVIDER)[3]\
-        .decode(AWKWARD_ENCODING).encode('ascii', 'ignore').split("\r\n")
+def parse(fcontents, utf16=False):
+    """Parse the file (as a string in fcontents).
+
+    Note that on disk, these files are written in UTF-16LE but once they
+    are stored in a transform spec, they have been converted to UTF-8.
+    """  # TODO where does this conversion take place??
+    if utf16:
+        # Handle a bunch of Unicode nonsense; files appear to be in UTF-16LE
+        quant_results = fcontents.split(BLOCK_DIVIDER_UTF16)[3]\
+            .decode(AWKWARD_ENCODING).encode('ascii', 'ignore').split("\r\n")
+    else:
+        quant_results = fcontents.split(BLOCK_DIVIDER)[3].split("\r\n")
 
     for res in quant_results:
         items = res.split("\t")
