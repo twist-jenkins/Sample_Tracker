105dc74c304f6b9ea053f39f8d28139a09a9b511
Kipp McMichael <kmcmichael@twistbioscience.com>
2016-02-18 15:52:43 -0800

    added column to list view; added requested data readout to spec view

--- app/static/source/js/controllers/transform-spec-view-spec.js
+++ app/static/source/js/controllers/transform-spec-view-spec.js
@@ -19,6 +19,9 @@ angular.module('twist.app').controller('transformSpecViewSpecController', ['$sco
         if (!$scope.selectedSpec) {
             $scope.specLoading = true;
             Api.getTransformSpec(specId).success(function (data) {
+
+                console.log(data.data);
+
                 $scope.specLoading = false;
                 var thisSpec = data.data;
                 thisSpec.plan = thisSpec.data_json;
