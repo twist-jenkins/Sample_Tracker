9a51077bf75f30b1f8312588b545b1f73e356e48
Kipp McMichael <kmcmichael@twistbioscience.com>
2016-02-18 18:05:13 -0800

    basic barcode validation service

--- app/static/source/js/services.js
+++ app/static/source/js/services.js
@@ -40,6 +40,14 @@ app = angular.module('twist.app')
             ,DATA_TYPE_RADIO: 'radio'
             ,DATA_TYPE_CSV: 'csv'
             ,BARCODE_PREFIX_PLATE: 'PLT'
+            ,BARCODE_TYPE_PLATE: 'PLATE'
+            ,BARCODE_TYPE_INSTRUMENT: 'INSTRUMENT'
+            ,BARCODE_TYPE_CARRIER: 'CARRIER'
+            ,INSTRUMENT_TYPE_HAMILTON: 'HAMILTON'
+            ,INSTRUMENT_TYPE_SEQUENCER: 'SEQUENCER'
+            ,INSTRUMENT_TYPE_ECHO: 'ECHO'
+            ,INSTRUMENT_TYPE_THERMOCYCLER: 'THERMOCYCLER'
+
         };
     }]
 )
@@ -1648,6 +1656,40 @@ app = angular.module('twist.app')
                 var forgotten = closure(memory[name]);
                 delete memory[name];
                 return forgotten;
+            }
+        };
+    }
+])
+
+.factory('BarcodeManager', ['Constants', 
+    function (Constants) {
+
+        var barcodePrefixes = {
+            'p': Constants.BARCODE_TYPE_PLATE
+            ,'i': Constants.BARCODE_TYPE_INSTRUMENT
+            ,'c': Constants.BARCODE_TYPE_CARRIER
+        };
+
+        var instruments = {
+            'iHAM04': Constants.INSTRUMENT_TYPE_HAMILTON
+        };
+
+        var carriers = {
+
+        };
+
+        return {
+            validateType: function (barcode, expectedType) {
+                if (barcodePrefixes[barcode.charAt(0)] == expectedType) {
+                    return true;
+                }
+                return false;
+            }
+            ,validateInstrument: function (barcode, expectedInstrumentType) {
+                if (instruments[barcode] == expectedInstrumentType) {
+                    return true
+                }
+                return false;
             }
         };
     }
